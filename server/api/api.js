dc905e64c39b6afe44b39464f38f93c15dca0fea
Baqar Ali <baqar.ali@sybrid.com>
2022-05-25 16:27:24 +0500

    commit

--- server/api/api.js
+++ server/api/api.js
@@ -2,6 +2,10 @@ const router = require("express").Router();
 
 router.use("/users", require("./users/userRoutes"));
 router.use("/orders", require("./orders/orderRoutes"));
-// router.use("/customers", require("./customers/customerRoutes"));
+router.use("/customers", require("./customers/customerRoutes"));
+router.use("/format", require("./formats/formatRoutes"));
+router.use("/patchcategory", require("./patchCategory/patchCategoryReoutes"));
+router.use("/placement", require("./placement/placementRoutes"));
+router.use("/unit", require("./units/unitsRoutes"));
 
 module.exports = router;
