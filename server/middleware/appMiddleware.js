89d36ca74b7732f3ed20196d53cc70fb9f19690f
syed.arbaz <syed.arbaz@DESKTOP-0AAC4DC>
2022-08-14 03:15:37 +0500

    CORS issue

--- server/middleware/appMiddleware.js
+++ server/middleware/appMiddleware.js
@@ -8,7 +8,12 @@ var fileUpload = require("express-fileupload");
 module.exports = function (app) {
   // Application Middlewares
   app.use(morgan("dev"));
-  app.use(cors());
+  app.use(
+    cors({
+      origin: "http://45.14.194.131:3000",
+      optionsSuccessStatus: 200, // For legacy browser support
+    })
+  );
   app.use(bodyParser.json({ limit: "50mb" })); // for parsing application/json
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(fileUpload());
