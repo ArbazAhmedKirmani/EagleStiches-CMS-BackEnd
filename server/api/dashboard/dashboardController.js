90c45b99906f0e675b01e1a52c65c6e9cfb21f1a
syed.arbaz <syed.arbaz@DESKTOP-0AAC4DC>
2022-09-05 01:05:37 +0500

    Dashboard controller changes

---
+++ server/api/dashboard/dashboardController.js
@@ -0,0 +1,143 @@
+const Unit = require("./unitsModel");
+const Order = require("../orders/orderModel");
+
+// exports.createUnit = async (req, res) => {
+//   try {
+//     const { unitName } = req.body;
+//     const unit = new Unit({
+//       unitName,
+//     });
+//     await unit.save();
+//     res.status(200).send({
+//       status: "Ok",
+//       message: "record created successfully",
+//     });
+//   } catch (err) {
+//     console.log("Error :", err);
+//     res.status(400).send({ status: "Error", message: "check server logs" });
+//   }
+// };
+
+exports.getAllDashboardData = async (req, res) => {
+  try {
+    let findQuery = { ...JSON.parse(req.query.data) };
+    let top = 10;
+    let skip = 0;
+    let populate = "";
+    let sort = "";
+
+    if (req.query.name) {
+      let regex = new RegExp(req.query.name);
+      findQuery.name = { $regex: regex };
+    }
+    if (req.query.populate) {
+      populate = req.query.populate;
+    }
+
+    if (req.query.top) {
+      top = parseInt(req.query.top);
+    }
+    if (req.query.skip) {
+      skip = parseInt(req.query.skip);
+    }
+
+    if (req.query.sort) {
+      sort = req.query.sort;
+    }
+    // let totalCount = await Unit.countDocuments({ ...findQuery });
+    const units = await Order.aggregate([
+      { $group: { totalAmount: { $sum: "totalPrice" } } },
+    ])
+    // ({ ...findQuery })
+    //   .populate(populate)
+    //   .skip(skip)
+    //   .limit(top)
+    //   .sort(sort);
+
+    res.status(200).send({
+      status: "Ok",
+      data: units,
+      count: totalCount,
+    });
+  } catch (err) {
+    console.log("Error :", err);
+    res.status(400).send({ status: "Error", message: "check server logs" });
+  }
+};
+
+// exports.getUnitById = async (req, res) => {
+//   try {
+//     const { id } = req.params;
+//     const unit = await Unit.findById({ _id: id });
+//     res.status(200).send({ status: "Ok", data: unit });
+//   } catch (err) {
+//     console.log("Error :", err);
+//     res.status(400).send({ status: "Error", message: "check server logs" });
+//   }
+// };
+
+// exports.updateUnitById = async (req, res) => {
+//   try {
+//     const { id } = req.params;
+//     const { unitName } = req.body;
+//     await Unit.findOneAndUpdate({ _id: id }, { unitName });
+
+//     let findQuery = {};
+//     let top = 10;
+//     let skip = 0;
+//     let populate = "";
+//     let sort = "";
+
+//     let totalCount = await Unit.countDocuments({ ...findQuery });
+//     const units = await Unit.find({ ...findQuery })
+//       .populate(populate)
+//       .skip(skip)
+//       .limit(top)
+//       .sort(sort);
+
+//     res.status(200).send({
+//       status: "Ok",
+//       message: "record updated successfully",
+//       data: units,
+//       count: totalCount,
+//     });
+//   } catch (err) {
+//     console.log("Error :", err);
+//     res.status(400).send({ status: "Error", message: "check server logs" });
+//   }
+// };
+
+// exports.deleteUnitById = async (req, res) => {
+//   try {
+//     const { id } = req.params;
+//     const order = await Order.findOne({ unit: id });
+//     if (order) {
+//       res.status(400).send({ status: "Error", message: "record exist!" });
+//     } else {
+//       await Unit.findByIdAndDelete({ _id: id });
+
+//       let findQuery = {};
+//       let top = 10;
+//       let skip = 0;
+//       let populate = "";
+//       let sort = "";
+
+//       let totalCount = await Unit.countDocuments({ ...findQuery });
+//       const units = await Unit.find({ ...findQuery })
+//         .populate(populate)
+//         .skip(skip)
+//         .limit(top)
+//         .sort(sort);
+
+//       res.status(200).send({
+//         status: "Ok",
+//         message: "record deleted successfully",
+//         data: units,
+//         count: totalCount,
+//       });
+//     }
+//   } catch (err) {
+//     console.log("Error :", err);
+//     res.status(400).send({ status: "Error", message: "check server logs" });
+//   }
+// };
